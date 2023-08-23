f89374e7c67c81a62892227a887fe3f6705dad04
Abhishek40 <abhishekraj144@gmail.com>
2023-08-23 12:13:24 +0530

    updated AKS cluster

--- terraform-manifests/c1-versions.tf
+++ terraform-manifests/c1-versions.tf
@@ -1,4 +1,4 @@
-#Terraform Block
+# Terraform Block
 terraform {
   required_version = ">= 1.0.0"
   required_providers {
@@ -10,11 +10,28 @@ terraform {
         source = "hashicorp/random"
         version = ">= 3.0"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.0"
+    }
   }
+}
+
+# Terraform State Storage to Azure Storage Container
+backend "azurerm"{
+  resource_group_name   = "terraform-storage-rg"
+  storage_account_name  = "terraformstatekalyan"
+  container_name        = "tfstatefiles"
+  key                   = "dev.terraform.tfstate"
 }
 
 #Provider Block
 provider "azurerm" {
-  features {}
+  features {
+    # Updated as part of June2023 to delete "ContainerInsights Resources" when deleting the Resource Group
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
   skip_provider_registration = true
 }
--- terraform-manifests/c2-generic-variables.tf
+++ terraform-manifests/c2-generic-variables.tf
@@ -25,3 +25,30 @@ variable "resource_group_location" {
   description = "Resource group location"
   default = "eastus2"
 }
+
+# Azure Location
+variable "location" {
+  type = string
+  description = "Azure Region where all these resources will be provisioned"
+  default = "Central US"
+}
+
+# SSH Public Key for Linux VMs
+variable "ssh_public_key" {
+  default = "~/.ssh/aks-prod-sshkeys-terraform/aksprodsshkey.pub"
+  description = "This variable defines the SSH Public Key for Linux k8s Worker nodes"  
+}
+
+# Windows Admin Username for k8s worker nodes
+variable "windows_admin_username" {
+  type = string
+  default = "azureuser"
+  description = "This variable defines the Windows admin username k8s Worker nodes"  
+}
+
+# Windows Admin Password for k8s worker nodes
+variable "windows_admin_password" {
+  type = string
+  default = "StackSimplify@102"
+  description = "This variable defines the Windows admin password k8s Worker nodes"  
+}
